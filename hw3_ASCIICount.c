60fb5170f417aca2dccd37d6c242abf81142ebb9
nemimccarter <nemimccarter@gmail.com>
2017-10-02 14:57:54 -0700

    Add file i/o

---
+++ hw3_ASCIICount.c
@@ -0,0 +1,31 @@
+// Nehemya McCarter-Ribakoff
+// Yue
+// CSC 415 MW 4:10 PM
+// HW1: Hello World
+// 4 October 2017
+
+/* NOTES: each thread gets its own array
+*         to count ASCII instances. Sum
+*         the instances of each into a new
+*         array after every thread is done
+*/
+
+#include <stdio.h>
+#define NUM_THREADS 8
+
+
+// global 64KB array buffer
+char buffer[65536];
+
+int main(int argc, char *argv[]) {
+FILE *filePointer;
+
+filePointer = fopen(argv, "r");
+if (!filePointer) {
+    printf("Error: Cannot open file. Exiting.\n";
+    exit(1);
+}
+
+
+    return 0;
+}
